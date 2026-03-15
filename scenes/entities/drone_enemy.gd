e1860e818769e80d82b7c0d7ba3fa9b99bd6558e
ag616 <adit0616@gmail.com>
2026-03-15 13:50:53 -0500

    Figuring out pathfinding
    Need to fix the navigation layer

--- scenes/entities/drone_enemy.gd
+++ scenes/entities/drone_enemy.gd
@@ -4,6 +4,8 @@ var drone_speed:int = 50 #speed of the drone
 var mark_enemy:bool = false #flag to see if the enemy is marked
 var target: CharacterBody2D = null #make a variable for characterbody2d object - THIS ALWAYS CREATES A REFERENCE
 
+@onready var nav_agent := $NavigationAgent2D as NavigationAgent2D
+
 func _on_agro_range_body_entered(body: Node2D) -> void:
 	mark_enemy = true
 	target = body #update the reference object - this stores the player node
@@ -16,7 +18,11 @@ func _on_agro_range_body_exited(_body: Node2D) -> void:
 func _physics_process(_delta: float) -> void:
 	#need to move towards player
 	if mark_enemy:
-		drone_dir = (target.global_position - global_position).normalized() #get the PV 
+		#drone_dir = (target.global_position - global_position).normalized() #get the PV 
+		drone_dir = to_local(nav_agent.get_next_path_position()).normalized()
 	velocity = drone_dir*drone_speed
 	move_and_slide()
 	
+func _on_nav_refresh_timer_timeout() -> void:
+	if mark_enemy:
+		nav_agent.target_position = target.global_position
