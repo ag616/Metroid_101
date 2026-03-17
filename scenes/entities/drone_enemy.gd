42db840e3e2a1ea5a70249413d8cf87d238f1059
ag616 <adit0616@gmail.com>
2026-03-16 22:07:00 -0500

    Patroling implemented
    The enemy always goes back to home to patrol

--- scenes/entities/drone_enemy.gd
+++ scenes/entities/drone_enemy.gd
@@ -3,8 +3,22 @@ var drone_dir: Vector2 = Vector2.ZERO #direction of the drone
 var drone_speed:int = 50 #speed of the drone
 var mark_enemy:bool = false #flag to see if the enemy is marked
 var target: CharacterBody2D = null #make a variable for characterbody2d object - THIS ALWAYS CREATES A REFERENCE
+var patrol_positions: Array[Vector2] =[]
+
 
 @onready var nav_agent := $NavigationAgent2D as NavigationAgent2D
+
+func _ready() -> void:
+	for marker in $PatrolMarkers.get_children():
+		if marker is Marker2D:
+			patrol_positions.append(marker.global_position) #update marker positions
+
+func update_marker_positions():
+	var i =0
+	for marker in $PatrolMarkers.get_children():
+		patrol_positions[i] = marker.global_position
+		i += 1
+	
 
 func _on_agro_range_body_entered(body: Node2D) -> void:
 	mark_enemy = true
@@ -22,10 +36,7 @@ func _physics_process(_delta: float) -> void:
 		drone_dir = to_local(nav_agent.get_next_path_position()).normalized()
 	else:
 		idle_patrol()
-		if nav_agent.distance_to_target() <= 50:
-			drone_dir = to_local(nav_agent.get_next_path_position()).normalized()
-		else: 
-			drone_dir = Vector2.ZERO
+		drone_dir = to_local(nav_agent.get_next_path_position()).normalized()
 	velocity = drone_dir*drone_speed
 	move_and_slide()
 	
@@ -34,6 +45,6 @@ func _on_nav_refresh_timer_timeout() -> void:
 		nav_agent.target_position = target.global_position
 
 func idle_patrol():
-	var patrol_point = $PatrolMarkers.get_children().pick_random() as Marker2D
+	var patrol_point:Vector2 = patrol_positions.pick_random()
 	if !mark_enemy and nav_agent.is_navigation_finished():
-		nav_agent.target_position = patrol_point.global_position
+		nav_agent.target_position = patrol_point
