6bcecf8506292f4d175b144012d866366fd579ba
ag616 <adit0616@gmail.com>
2026-03-17 21:20:32 -0500

    idling and patroling

--- scenes/entities/drone_enemy.gd
+++ scenes/entities/drone_enemy.gd
@@ -1,10 +1,12 @@
 extends CharacterBody2D
 var drone_dir: Vector2 = Vector2.ZERO #direction of the drone
-var drone_speed:int = 50 #speed of the drone
+var chasing_speed:int = 50 #speed of the drone while chasing
 var mark_enemy:bool = false #flag to see if the enemy is marked
 var target: CharacterBody2D = null #make a variable for characterbody2d object - THIS ALWAYS CREATES A REFERENCE
 var patrol_positions: Array[Vector2] =[]
 var chasing_distance: float = 200 #this is the chasing distance
+var idle_speed: int = 25 #speed of drone while idling
+var drone_speed: int = 0 #speed of the drone
 
 
 @onready var nav_agent := $NavigationAgent2D as NavigationAgent2D
@@ -22,10 +24,13 @@ func update_marker_positions():
 	
 
 func _on_agro_range_body_entered(body: Node2D) -> void:
+	drone_speed = chasing_speed
 	mark_enemy = true
 	target = body #update the reference object - this stores the player node
+	nav_agent.target_position = target.global_position
 
 func _on_agro_range_body_exited(_body: Node2D) -> void:
+	drone_speed = idle_speed
 	mark_enemy = false
 	nav_agent.target_position = patrol_positions.pick_random()
 	
@@ -33,6 +38,7 @@ func _on_agro_range_body_exited(_body: Node2D) -> void:
 func _physics_process(_delta: float) -> void:
 	#need to move towards player
 	if mark_enemy and (nav_agent.get_path_length() <= chasing_distance):
+		drone_speed = chasing_speed
 		#drone_dir = (target.global_position - global_position).normalized() #get the PV 
 		drone_dir = to_local(nav_agent.get_next_path_position()).normalized()
 	else:
@@ -47,6 +53,7 @@ func _on_nav_refresh_timer_timeout() -> void:
 		nav_agent.target_position = target.global_position
 
 func idle_patrol():
+	drone_speed = idle_speed
 	var patrol_point:Vector2 = patrol_positions.pick_random()
 	if !mark_enemy and nav_agent.is_navigation_finished():
 		nav_agent.target_position = patrol_point
